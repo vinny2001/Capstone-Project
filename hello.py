5774e3e9ea05f6e5591a42a20fd4c5c6eee0f2ac
vinny2001 <30963796+vinny2001@users.noreply.github.com>
2019-06-19 21:14:04 -0400

    Add files via upload
    Initial exposure to python and basic code strings

---
+++ hello.py
@@ -0,0 +1,19 @@
+# Variables
+Secret_Message = "Bush Did 9/11"
+Person = 'Bush'
+Reason = 'To steal oil from the Arabs'
+Number = 911
+Code = 'Red'
+
+# Output
+if Code == 'Red':
+
+    if Number == 911:
+        print('But the Arabs truly did nothing')
+
+    if Number < 911:
+        print('Bush Blamed the Arabs for 9/11')
+
+if Code == 'Green':
+
+    print("9/11 Didn't happen")
