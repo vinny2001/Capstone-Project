420ff5b106cb1e46406ce63db7f36da5566ff73e
vinny2001 <30963796+vinny2001@users.noreply.github.com>
2019-06-24 11:34:50 -0400

    Add files via upload

--- Downloading Images  From Internet in a random sequence by defining variables.py
+++ Downloading Images  From Internet in a random sequence by defining variables.py
@@ -1,5 +1,11 @@
+# This Revision of the Image Downloader has printed text in the CMD window to display the download progress to the user
 import urllib.request
 import random
+import time
+print("""*********************************************************************************************************************
+Establishing Internet Connection
+""")
+time.sleep(2)
 
 
 def download_image_url(url):
@@ -8,4 +14,15 @@ def download_image_url(url):
     urllib.request.urlretrieve(url, fullname)
 
 
+print("""Download has started
+""")
+time.sleep(2)
 download_image_url("https://www.esa.int/var/esa/storage/images/esa_multimedia/images/2018/03/italy_and_mediterranean/17402074-1-eng-GB/Italy_and_Mediterranean_node_full_image_2.jpg")
+if download_image_url:
+    print("""Done!
+    """)
+time.sleep(1)
+print("""Coded by Vincenzo D'Aria. Enjoy!"
+*********************************************************************************************************************
+""")
+time.sleep(2)
